a8a3524250993515a5b30fb745ad69e2d46af2fb
HARVEYNASH\dungphamt5 <dungphamt5@nashtechglobal.com>
2023-05-01 17:01:29 +0700

    update response

--- ci_cd_1/Controllers/TestController.cs
+++ ci_cd_1/Controllers/TestController.cs
@@ -24,16 +24,17 @@ namespace ci_cd_1.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public string Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            // var rng = new Random();
+            // return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            // {
+            //     Date = DateTime.Now.AddDays(index),
+            //     TemperatureC = rng.Next(-20, 55),
+            //     Summary = Summaries[rng.Next(Summaries.Length)]
+            // })
+            // .ToArray();
+            return "Hello world!!";
         }
     }
 }
